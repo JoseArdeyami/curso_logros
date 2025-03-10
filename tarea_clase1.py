1e80cf77731c3053a75d953af61745d13da7a12c
JoseArdeyami <villa.ardeyami@gmail.com>
2025-03-10 11:17:16 -0400

    Add files via upload

---
+++ tarea_clase1.py
@@ -0,0 +1,10 @@
+nombre = "Jose"
+ciudad = "Maracaibo"
+edad =  25
+
+d1 = 10
+d2 = 8
+d3 = 6
+d4 = (d1+d2+d3)//3
+
+print("Mi nombre es ", nombre, "yo soy de ", ciudad, "y tengo ", edad, "años\n", "Mi promedio total es: ", d4, "sobre 10")
