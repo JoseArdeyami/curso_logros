62894be423fbfe074b3c345865b1303e4201bc9d
JoseArdeyami <villa.ardeyami@gmail.com>
2025-03-12 09:26:57 -0400

    Add files via upload

---
+++ lista de estudiantes.py
@@ -0,0 +1,8 @@
+clase_programacion = ["Jose Villalobos", "Jose Gonzales", "Jose Perozo", "Juan Hidalgo", "Genesis paz", "Raquel Carruyo", "Paula Carruyo", "Alberto Mendez", "Hebert Rondon", "Alicia Marcano", "Gabriel Barrera", "Santiago Salas", "Adan Cubillan", "Angel medina"]
+
+print(clase_programacion)
+
+clase_programacion[10] = "Daniel Espitia"
+
+print(clase_programacion)
+
