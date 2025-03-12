d2dabd5b361b6501ef72c0691b1a507ed51a65d8
JoseArdeyami <villa.ardeyami@gmail.com>
2025-03-12 10:08:38 -0400

    Add files via upload

---
+++ Diccionario.py
@@ -0,0 +1,9 @@
+nombre = input("Escriba su nombre")
+apellido = input("Escriba su apellido")
+edad = int(input("Escriba su edad"))
+pais = input("Escriba su pais")
+peliculaFav = input("Escriba su pelicula favorita")
+alumno1 = {
+"nombre":nombre,"apellido":apellido,"edad": edad,"pais": pais, "peliculaFav":peliculaFav}
+
+print(alumno1)
